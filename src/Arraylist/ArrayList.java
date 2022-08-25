be73119ee8c1497636d1c67522dcd9138bfaf8a5
DESKTOP-TPQHNMR\baek <tmdduq5498@gmail.com>
2022-08-25 21:59:44 +0900

    addLast()

---
+++ src/Arraylist/ArrayList.java
@@ -0,0 +1,16 @@
+package Arraylist;
+
+public class ArrayList {
+    private int size = 0;
+    private Object[] elementData = new Object[100];
+
+    public boolean addLast(Object element){
+
+        elementData[size] = element;
+        size++;
+
+        return true;
+    }
+
+
+}
