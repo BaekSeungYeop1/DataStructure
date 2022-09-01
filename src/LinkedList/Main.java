19a02cfb323fe7d0d08ab94ef0aa670ef57542ca
DESKTOP-TPQHNMR\baek <tmdduq5498@gmail.com>
2022-09-01 18:54:19 +0900

    LinkedList - Iterator remove()

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -7,9 +7,6 @@ public class Main {
         numbers.addLast(20);
         numbers.addLast(30);
         LinkedList.ListIterator i = numbers.listIterator();
-        i.add(5);
-        i.next();
-        i.add(15);
-        System.out.println(numbers);
+        i.remove();
     }
 }
