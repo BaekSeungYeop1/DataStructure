bcd514d186a97b1f73b55919daf315fea5c1aa72
DESKTOP-NT05U6L\Yeop <tmdduq5498@gmail.com>
2022-09-05 23:04:55 +0900

    스택 추가

---
+++ src/Stack/StackPractice.java
@@ -0,0 +1,23 @@
+package Stack;
+
+import java.util.Stack;
+
+public class StackPractice {
+    public static void main(String args[]){
+        Stack<Integer> stack = new Stack<>();
+
+// 데이터 삽입
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+// 데이터 출력
+        stack.peek(); // 3 출력 (맨 위의 값 출력)
+
+// 데이터 삭제
+        stack.pop(); // 3 제거
+        stack.pop(); // 2 제거
+        stack.pop(); // 1 제거
+        stack.clear(); // 모든 데이터 제거
+    }
+}
