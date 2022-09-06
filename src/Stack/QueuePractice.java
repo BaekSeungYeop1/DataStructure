2a82b43672790249aa6c715c1c732db900ed0a61
DESKTOP-NT05U6L\Yeop <tmdduq5498@gmail.com>
2022-09-06 22:52:11 +0900

    Queue

---
+++ src/Stack/QueuePractice.java
@@ -0,0 +1,23 @@
+package Stack;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueuePractice {
+    public static void main(String args[]){
+        Queue<Integer> q = new LinkedList<>();
+
+// 데이터 삽입
+        q.offer(1);
+        q.offer(2);
+        q.offer(3);
+
+// 데이터 출력& 삭제
+        q.peek(); // 1 출력 (맨 위의 값 출력)
+        q.poll(); // 1 출력 후 삭제
+
+// 데이터 삭제
+        q.remove(); // 2 삭제
+        q.clear(); // 모든 데이터 삭제
+    }
+}
